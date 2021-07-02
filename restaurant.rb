3d0919f09f2a9d09a4ca6eb728fca7442bb3e786
Diane Phuong Nguyen <dianenguyen@gmail.com>
2021-07-02 00:48:41 +0000

    Automatically backed up by Learn

---
+++ restaurant.rb
@@ -0,0 +1,19 @@
+def restaurant
+  restaurant_name = "Guy's American Kitchen & Bar"
+  cuisine = "american"
+  motto = "Welcome to Flavor Town!"
+end
+
+def print_name
+  puts "Guy Fieri"
+end
+
+def sum(num1, num2)
+  num1 + num2
+end
+
+puts restaurant
+print_name
+
+puts sum(num1, num2)
+puts sum sum1, sum2
---
+++ restaurant2.rb
@@ -0,0 +1,13 @@
+def print_and_return_name
+  puts "Guy Fieri"
+  # "Guy Fieri"
+end
+
+def give_me_five
+  return puts 5
+end
+
+puts print_and_return_name
+
+puts give_me_five
+puts give_me_five
